117fc7f269cc87656d36fab237cdd1a8c4d4922b
TaXueWWL <1210812591@qq.com>
2018-12-05 13:50:48 +0800

    snowalker--update demo

--- shleld-ratelimter-demo/src/main/java/com/snowalker/ratelimiter/demo/controller/TestController.java
+++ shleld-ratelimter-demo/src/main/java/com/snowalker/ratelimiter/demo/controller/TestController.java
@@ -1,6 +1,5 @@
 package com.snowalker.ratelimiter.demo.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.snowalker.shield.ratelimiter.core.annotation.RateLimiter;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.slf4j.Logger;
@@ -25,10 +24,17 @@ public class TestController {
 
     @ResponseBody
     @RequestMapping("ratelimiter")
-    @RateLimiter(key = "ratedemo:1.0.0", limit = 5, expire = 100, message = MESSAGE)
+    @RateLimiter(key = "ratedemo:1.0.0", limit = 5, expire = 10, message = MESSAGE)
     public String sendPayment(HttpServletRequest request) throws Exception {
 
         return "正常请求";
     }
 
+    @ResponseBody
+    @RequestMapping("ratelimiter1")
+    @RateLimiter(key = "ratedemo:1.0.1", limit = 5, expire = 10, message = MESSAGE)
+    public String sendPayment1(HttpServletRequest request) throws Exception {
+
+        return "正常请求";
+    }
 }
